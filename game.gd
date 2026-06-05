f099454c18096cc4e5eeb74c8c1fdf776c8c1e89
Atharv Dhiman <atharv.emails@gmail.com>
2026-06-05 23:43:42 +0400

    added tree spawning

--- game.gd
+++ game.gd
@@ -9,10 +9,28 @@ func spawn_mob():
 	add_child(new_mob)
 
 
+func spawn_tree():
+	var new_tree = preload("res://Pine_tree.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_tree.global_position = %PathFollow2D.global_position
+	add_child(new_tree)
+
+
 func _on_timer_timeout() -> void:
 	spawn_mob()
+
 
 
 func _on_player_health_depleted() -> void:
 	%GameOver.visible = true   #displays the Game over screen
 	get_tree().paused = true    #pauses the whole game
+
+
+func _on_restart_game_pressed() -> void:
+	get_node("Player")
+	get_tree().paused = false
+	%GameOver.visible = false
+
+
+func _on_tree_spawn_timer_timeout() -> void:
+	spawn_tree()
